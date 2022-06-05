9334899e17f0996635b74b1f7d9ca61d5d367117
AlysonSSantos <alysonsantos080@gmail.com>
2022-06-04 21:00:03 -0300

    #002 - calculator

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,27 +2,94 @@ var button = document.querySelectorAll(".button-single")
 var display = document.querySelector('.display')
 var equalButton = document.querySelector("#equalFunctionButton")
 functionButtons = document.querySelectorAll('.function-buttons')
+var eraseButton = document.getElementById("eraseButton")
 
-const pressingButtons = (numero) =>{
-    console.log(numero);
+const pressingButtons = (number) =>{
+    console.log(number);
     
     if(display.value == 0){
-        display.value = numero;
+        display.value = number;
     }else{
-        display.value += numero;
+        display.value += number;
     }
 
     for(var i = 0; i < button.length; i++){
     
-        if(display.value.length > 12){
+        if(display.value.length >= 20){
             button[i].style.pointerEvents="none"
-            equalButton.style.pointerEvents='all'
+        } else{
+            button[i].style.pointerEvents="all"
+            
         }
 
     }
 
 }
 
+window.addEventListener('keydown',(event)=>{
+
+    if(display.value.length < 20){
+        keysDown();
+    }else{
+        if(event.key == '+' || event.key == '-' || event.key == '*' || event.key == '/'){
+            attribute = event.key;
+            pressingButtons(attribute);
+        } 
+    
+        switch(event.key){
+            case '=':
+                attribute = '=';
+                pressingFunctionButtons(attribute);
+            break;
+            case 'Backspace':
+                attribute = '<';
+                pressingFunctionButtons(attribute);
+            break;
+            case 'Enter':
+                attribute = '=';
+                pressingFunctionButtons(attribute);
+            break;
+            case 'c':
+                attribute = 'c';
+                pressingFunctionButtons(attribute);
+            break;
+        }
+    }
+
+    function keysDown(){
+
+    if(event.key == 1 || event.key == 2 || event.key == 3 || event.key == 4 || event.key == 5 || event.key == 6 || event.key == 7 || event.key == 8 || event.key == 9 || event.key == 0|| event.key == '.'){
+        number = event.key;
+        pressingButtons(number);
+    }
+
+    if(event.key == '+' || event.key == '-' || event.key == '*' || event.key == '/'){
+        attribute = event.key;
+        pressingButtons(attribute);
+    } 
+
+    switch(event.key){
+        case '=':
+            attribute = '=';
+            pressingFunctionButtons(attribute);
+        break;
+        case 'Backspace':
+            attribute = '<';
+            pressingFunctionButtons(attribute);
+        break;
+        case 'Enter':
+            attribute = '=';
+            pressingFunctionButtons(attribute);
+        break;
+        case 'c':
+            attribute = 'c';
+            pressingFunctionButtons(attribute);
+        break;
+    }
+
+    }
+
+})
 
 const pressingFunctionButtons = (attribute) =>{
     switch(attribute){
@@ -40,11 +107,22 @@ const pressingFunctionButtons = (attribute) =>{
         break;
         case '<' :
             let numberErase = display.value.substring(0, display.value.length-1)
-
+            
             display.value=numberErase;
 
             if(display.value == ''){
                 display.value = '0'
+            }
+
+            for(var i = 0; i < button.length; i++){
+
+                if(display.value.length >= 12){
+                    button[i].style.pointerEvents="none"
+                } else{
+                    button[i].style.pointerEvents="all"
+                    
+                }
+
             }
         break;
         case 'c' :
